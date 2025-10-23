afadbd21bbac6b5ff576624077b4542940dd012d
Justine <tayting05@gmail.com>
2025-10-23 18:44:40 +0800

    Create
    Removed the problem statement comment to streamline the code.

---
+++ source-code/midterm-lab1-problem1.py
@@ -0,0 +1,8 @@
+full_name = "John Doe"
+_email = "john.doe@example.com"
+university_name = "ABC University"
+print("Problem 1")
+print("Database Record")
+print("Name: \t\t\t", full_name)
+print("Email: \t\t\t", _email)
+print("University: \t", university_name)
