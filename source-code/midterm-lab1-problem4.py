0f58e69f2614cfeb85b7b23d370c2d34ea724325
Justine <tayting05@gmail.com>
2025-10-24 12:06:56 +0800

    Update midterm-lab1-problem4.py

--- source-code/midterm-lab1-problem4.py
+++ source-code/midterm-lab1-problem4.py
@@ -1,16 +1,18 @@
+day = int(input("Enter Day number (1-7): "))
+
 if day == 1:
- print("Monday")
+    print("Monday")
 elif day == 2:
- print("Tuesday")
+    print("Tuesday")
 elif day == 3:
- print("Wednesday")
-  elif day == 4:
- print("Thursday")
+    print("Wednesday")
+elif day == 4:
+    print("Thursday")
 elif day == 5:
- print("Friday")
+    print("Friday")
 elif day == 6:
- print("Saturday")
+    print("Saturday")
 elif day == 7:
- print("Sunday")
+    print("Sunday")
 else:
- print("Invalid input")
+    print("Invalid input")
