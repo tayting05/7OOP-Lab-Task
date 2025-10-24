db749db036cb97905275b2e0703c96d6ea345716
Justine <tayting05@gmail.com>
2025-10-24 12:10:38 +0800

    Update midterm-lab4-problem1.py

--- source-code/midterm-lab4-problem1.py
+++ source-code/midterm-lab4-problem1.py
@@ -1,33 +1,38 @@
 menu = {
- "Water": 15,
- "Burger": 50,
- "Spaghetti": 75,
- "Soda": 25,
- "Chicken": 60,
- "Steak": 85,
- "Nuggets": 50,
- "Fries": 40,
- "Juice": 25,
- "Pie": 45
+    "Water": 15,
+    "Burger": 50,
+    "Spaghetti": 75,
+    "Soda": 25,
+    "Chicken": 60,
+    "Steak": 85,
+    "Nuggets": 50,
+    "Fries": 40,
+    "Juice": 25,
+    "Pie": 45
 }
+
 cart = []
 prices = []
+
 print("-------- MENU --------")
 for item, price in menu.items():
- print(f"{item:15}: ₱{price}")
+    print(f"{item:15}: ₱{price}")
+
 while True:
- order = input("Enter a food to buy (q to quit): ").strip().title()
- if order.lower() == "q":
- break
- elif order in menu:
- cart.append(order)
- prices.append(menu[order])
- print(f"Added {order} - ₱{menu[order]}")
- else:
- print("Not Available")
+    order = input("Enter a food to buy (q to quit): ").strip().title()
+    if order.lower() == "q":
+        break
+    elif order in menu:
+        cart.append(order)
+        prices.append(menu[order])
+        print(f"Added {order} - ₱{menu[order]}")
+    else:
+        print("Not Available")
+
 print("\n----- YOUR ORDERS -----")
 total = 0
 for item, price in zip(cart, prices):
- print(f"{item:15} ₱{price}")
- total += price
+    print(f"{item:15} ₱{price}")
+    total += price
+
 print(f"\nYour total is: ₱{total}")
